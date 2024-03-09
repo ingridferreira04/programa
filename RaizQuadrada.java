ca80feaac4b484b0f0eb78e5a36ca384fbaeec51
ingrid <824113792@ulife.com.br>
2024-03-08 21:33:47 -0300

    yre

--- RaizQuadrada.java
+++ RaizQuadrada.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-
+//
 public class RaizQuadrada {
     public static void main(String[]args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("Digite um Número");
-    double Número = sc.nextDouble();
+    double Número = sc.nextDouble(); 
     System.out.println(Math.sqrt(Número));
     sc.close();
     }
